1d774adb1b3e31c6693602e74ae55a9b1a628693
tinosam <yvestinos@gmail.com>
2018-04-09 17:06:30 +0100

    book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
 # write your code here
+	def title=(a)
+		w = a.split(" ")
+		for i in 0..(w.length-1) do
+			if i == 0
+				w[i].capitalize!
+			elsif w[i] != "and" && w[i] !="the" && w[i] !="over" && w[i] !="a" && w[i] !="an" && w[i] !="in" && w[i] !="of"
+				w[i].capitalize!
+			end
+		end
+		@title=w.join(" ")
+	end
+
+	def title
+		@title
+	end
 end
